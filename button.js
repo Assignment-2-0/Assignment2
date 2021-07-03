59060474f63a5363594ef675216797173d45fa15
adamsua0 <85155444+adamsua0@users.noreply.github.com>
2021-07-03 20:59:22 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,18 @@
-function showAlert() {
+function validateFirstName(){
+    var fname = document .getElementById("firstName") .nodeValue;
+    var input = /[a-zA-Z]{3-15}/;
+    if (input.test(fname)){document .getElementById("firstNamePrompt").style.color="green";
+                           document .getElementById("firstNamePrompt").innerHTML = "valid";return true}
+    else{document .getElementById("firstNamePrompt").style.color="red";
+         document .getElementById("firstNamePrompt").innerHTML="invalid";return false}
+}
+function validateLastName(){
+    var fname = document .getElementById("lastName") .nodeValue;
+    var input = /[a-zA-Z]{3-15}/;
+    if (input.test(lname)){document .getElementById("lastNamePrompt").style.color="green";
+                           document .getElementById("lastNamePrompt").innerHTML = "valid";return true}
+    else{document .getElementById("lastNamePrompt").style.color="red";
+         document .getElementById("lastNamePrompt").innerHTML="invalid";return false}
+}function showAlert() {
     alert("Thank you for your consumption.");
  }
