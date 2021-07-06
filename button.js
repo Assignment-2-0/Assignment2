c5434ee825296ff4c0a941634e96915ed78f9f55
Limsonghaw <85155296+Limsonghaw@users.noreply.github.com>
2021-07-06 20:15:06 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,18 +1,22 @@
-function validateFirstName(){
-    var fname = document .getElementById("firstName") .nodeValue;
+function showAlert() {
+    alert("Thank you for your consumption.");
+ }
+ function validateFirstName(){
+    var fname = document .getElementById("firstName") .value;
     var input = /[a-zA-Z]{3-15}/;
-    if (input.test(fname)){document .getElementById("firstNamePrompt").style.color="green";
-                           document .getElementById("firstNamePrompt").innerHTML = "valid";return true}
+    if (input.test(fname)){
+        document .getElementById("firstNamePrompt").style.color="green";
+        document .getElementById("firstNamePrompt").innerHTML = "valid";
+        return true}
     else{document .getElementById("firstNamePrompt").style.color="red";
-         document .getElementById("firstNamePrompt").innerHTML="invalid";return false}
+         document .getElementById("firstNamePrompt").innerHTML="invalid";
+         return false}
 }
 function validateLastName(){
-    var fname = document .getElementById("lastName") .nodeValue;
+    var fname = document .getElementById("lastName") .value;
     var input = /[a-zA-Z]{3-15}/;
-    if (input.test(lname)){document .getElementById("lastNamePrompt").style.color="green";
+    if (input.test(fname)){document .getElementById("lastNamePrompt").style.color="green";
                            document .getElementById("lastNamePrompt").innerHTML = "valid";return true}
     else{document .getElementById("lastNamePrompt").style.color="red";
          document .getElementById("lastNamePrompt").innerHTML="invalid";return false}
-}function showAlert() {
-    alert("Thank you for your consumption.");
- }
+}
